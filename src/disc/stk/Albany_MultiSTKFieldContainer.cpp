06d4730e7344970d7b4dad3183379f7bb97c31ff
Luca Bertagna <lbertag@sandia.gov>
2020-05-05 18:05:09 -0600

    Uniformed MultiSTKFieldContainer template inst with the Ordinary one.

--- src/disc/stk/Albany_MultiSTKFieldContainer.cpp
+++ src/disc/stk/Albany_MultiSTKFieldContainer.cpp
@@ -3,8 +3,13 @@
 //    This Software is released under the BSD license detailed     //
 //    in the file "license.txt" in the top-level Albany directory  //
 //*****************************************************************//
+
 #include "Albany_MultiSTKFieldContainer.hpp"
 #include "Albany_MultiSTKFieldContainer_Def.hpp"
 
-MULTISTKFIELDCONTAINER_INSTANTIATE_TEMPLATE_CLASS(
-    Albany::MultiSTKFieldContainer)
+namespace Albany {
+
+template class MultiSTKFieldContainer<true>;
+template class MultiSTKFieldContainer<false>;
+
+}  // namespace Albany
